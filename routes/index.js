2ee8c32156b0968e96b3c62ee34b4ca546a1d178
陈曦 <chenxia@zhubajie.com>
2018-02-09 11:28:30 +0800

    静态页面

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@
  */
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers');
 
 router.get('/',  function(req, res, next) {
   res.render('index', { title: 'chenxiblog'});
