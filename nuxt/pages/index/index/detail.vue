c69efd1a4f6379b29e407b5adab4030c8421e588
陈曦 <chenxia@zhubajie.com>
2018-09-04 16:40:00 +0800

    modify

--- nuxt/pages/index/index/detail.vue
+++ nuxt/pages/index/index/detail.vue
@@ -4,8 +4,9 @@
             <div class="content">
                 <blog-breadcrumb :type="data.type" :title="data.title"></blog-breadcrumb>
                 <div class="blog-content">
-                    <h3 v-text="data.title" class="title"></h3>
+                    <h2 v-text="data.title" class="title"></h2>
                     <p><i class="el-icon-time"></i> {{momentFormat(data['created_at'])}}</p>
+                    <div class="hr"></div>
                     <div v-html="data.content"></div>
                 </div>
             </div>
